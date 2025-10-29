b49881368c2948b0e93199fc19238186ee153c81
Bayuajinugroho16 <aji13413@gmail.com>
2025-10-30 01:42:37 +0700

    bismillahamin

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,7 @@
-// src/config/constants.js
+
 export const API_BASE_URL = import.meta.env.VITE_API_URL?.replace(/\/+$/, '') || 'https://beckendflyio.vercel.app';
 export const WS_BASE_URL = import.meta.env.VITE_WS_URL || 'wss://beckendflyio.vercel.app/ws';
 export const BASE_URL = import.meta.env.VITE_API_URL?.replace('/api', '') || 'https://beckendflyio.vercel.app';
+
+console.log("✅ API_BASE_URL:", API_BASE_URL);
+console.log("✅ WS_BASE_URL:", WS_BASE_URL);
